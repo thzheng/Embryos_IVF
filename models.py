be39b2587b32a60c7358f867dfab6afe23dad041
Tianhao Zheng <zthzthzzz@gmail.com>
2019-05-13 00:13:39 -0700

    add resnet baseline

---
+++ models.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow.python.keras.applications import ResNet50
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Flatten, BatchNormalization
+
+def get_resnet50_baseline(output_classes):
+  model = Sequential()
+  model.add(ResNet50(include_top=False, pooling='avg', weights='imagenet', input_tensor=None, input_shape=None))
+  model.add(Flatten())
+  model.add(BatchNormalization())
+  model.add(Dense(output_classes, activation='softmax')) 
+  return model
---
+++ run.py
@@ -0,0 +1,5 @@
+from models import *
+
+model=get_resnet50_baseline(10)
+model.summary()
+
