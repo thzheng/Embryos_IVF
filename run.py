8e71633c8283d8872b5d38ad00f367f02d74cc89
Tianhao Zheng <zthzthzzz@gmail.com>
2019-05-14 21:52:29 -0700

    add a new model

--- run.py
+++ run.py
@@ -6,17 +6,22 @@ from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 import numpy as np
 
+model_type='my_model'
+# model_type='resnet'
+
 x_ori, y_ori = get_resized_images(224, './CS231Snapshot')
 assert len(x_ori) == len(y_ori)       
 
 # Prepare x
 x_ori = np.asarray(x_ori, dtype='float32')
-#  Change to Batch, Channel, W, H
-x_ori = np.swapaxes(x_ori, 1, 3)
-for n in range(x_ori.shape[0]):
-  x_ori[n] = prepare_resnet50(x_ori[n])
-#  Change to Batch, W, H, Channel
-x_ori = np.swapaxes(x_ori, 1, 3)
+
+if model_type=='resnet':
+  #  Change to Batch, Channel, W, H
+  x_ori = np.swapaxes(x_ori, 1, 3)
+  for n in range(x_ori.shape[0]):
+    x_ori[n] = prepare_resnet50(x_ori[n])
+  #  Change to Batch, W, H, Channel
+  x_ori = np.swapaxes(x_ori, 1, 3)
 
 #Prepare y
 class_int = dict()
@@ -38,17 +43,21 @@ x_train, x_test, y_train, y_test = train_test_split(x_ori, y_ori, test_size=0.2,
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=1)
 
 # Train
-MyModel=get_resnet50_baseline(15)
+if model_type=='resnet':
+  MyModel=get_resnet50_baseline(15)
+elif model_type=='my_model':
+  MyModel=get_my_model(15)
+
 MyModel.summary()
 MyAdam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, decay=0.0002)
 MyModel.compile(optimizer = MyAdam, loss = "categorical_crossentropy", metrics = ["accuracy"])
 MyEarlyStopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='min')
-MyMCP = ModelCheckpoint(".resnet.hdf5", save_best_only=True, monitor='val_loss', mode='min')
+MyMCP = ModelCheckpoint('.'+model_type+'.hdf5', save_best_only=True, monitor='val_loss', mode='min')
 MyReducelr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, epsilon=1e-4, mode='min')
 MyModel.fit(x = x_train, y = y_train, epochs = 50, batch_size = 16, verbose=1, callbacks=[MyEarlyStopping, MyMCP, MyReducelr], validation_data=(x_val, y_val))
 
 # Load the best check point
-MyModel.load_weights(filepath=".resnet.hdf5")
+MyModel.load_weights(filepath='.'+model_type+'.hdf5')
 score = MyModel.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
