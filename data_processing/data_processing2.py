0f9b5c7574c48d4d9391a9d5aa6bea2a250f829e
Xiangzhoujushi <tang.794@osu.edu>
2019-05-26 00:04:18 -0700

    processing hour

--- data_processing/data_processing2.py
+++ data_processing/data_processing2.py
@@ -1,25 +1,56 @@
 from PIL import Image
 import pytesseract
 import csv
+import re
+import os
 
-img = Image.open(
-	"../../EmbryoScopeAnnotatedData/Folder 1/D2019.03.12_S00011_I3205_P_WELL01/D2019.03.12_S00011_I3205_P_WELL01_RUN892.JPG"
-	).crop((726,770, 790, 790))
-img.save("../../EmbryoScopeAnnotatedData/Folder 1/try.jpg")
-text = pytesseract.image_to_string(img ,config ='--psm 6')
-# float(text)
-# print(float(text))
-print(text)
-
-def GetTimeFromImg():
-	pass
-
-"""
-yield all images with the original H,W and always 3 channel, and the label, folder and well
-(the 4th channel is always 255 for a fully saturated png, so we don't need it) 
-"""
-def yield_image(path, debug=0):
-	pass
+def GetTimeFromImg(path):
+	img = Image.open(path).crop((727,770, 790, 790))
+	# img.save("../../EmbryoScopeAnnotatedData/Folder 1/try.jpg")
+	text = pytesseract.image_to_string(img ,config ='--psm 6')
+	start_ind = 0
+	for i,ch in enumerate(text):
+		if ch.isdigit(): 
+			start_ind = i
+			break
+	new_text = text[i:]
+	return GetTimeFromText(new_text)
 
 
-yield_image("../../EmbryoScopeAnnotatedData")
+def GetTimeFromText(text = "", debug=0):
+	if text is "": return None
+	m = re.search(r'.*(?=h)', text)
+	if m:
+		label = m.group(0).strip()
+		try:
+			res = float(label)
+			return res
+		except:
+			if debug>0: print ("Error finding the label")
+			return None
+	else: 
+		return None
+
+# def yield_image(path, debug=0):
+# 	pass
+# yield_image("../../EmbryoScopeAnnotatedData")
+
+print(GetTimeFromImg('../../test_EmbryoScopeAnnotatedData/Folder 11/D2019.03.16_S00014_I3205_P_WELL02/D2019.03.16_S00014_I3205_P_WELL02_RUN016.JPG'))
+
+path = "../../test_EmbryoScopeAnnotatedData/"
+counter = 0
+for directory in os.listdir(path):
+	if directory[0] is ".": continue
+	file_path = os.path.join(path, directory)
+	for well in os.listdir(file_path):
+		if well[0] is ".": continue
+		full_path_to_well = os.path.join(file_path, well)
+		for img in os.listdir(full_path_to_well):
+			if img[0] is ".": continue
+			print(counter)
+			full_path_to_img = os.path.join(full_path_to_well, img)
+			print("current_image: " + full_path_to_img)
+			if GetTimeFromImg(full_path_to_img):
+				print(GetTimeFromImg(full_path_to_img))
+			else:
+				print("error")
